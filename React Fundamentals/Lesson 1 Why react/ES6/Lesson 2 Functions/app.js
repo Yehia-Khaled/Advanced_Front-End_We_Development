bc8cb46a1dadf003cac3a73b92f13aab1e151c9f
Yehia Khaled <55062155+Yehia-Khaled@users.noreply.github.com>
2021-09-06 17:57:22 +0200

    Lecture Using Arrow Functions Code and notes

--- React Fundamentals/Lesson 1 Why react/ES6/Lesson 2 Functions/app.js
+++ React Fundamentals/Lesson 1 Why react/ES6/Lesson 2 Functions/app.js
@@ -1,2 +1,8 @@
 'use strict';
-console.log('hi yehia')
+// console.log('hi yehia') //test File
+
+//Arrow Functions
+const upperizedNames = ['Farrin', 'Kagure', 'Asser'].map(
+    name => name.toUpperCase()
+);
+console.log(upperizedNames)
