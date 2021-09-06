f842f15cc56341069aa40a99a55b75697a9b0fee
Yehia Khaled <55062155+Yehia-Khaled@users.noreply.github.com>
2021-09-06 22:23:12 +0200

    Quiz: Convert Function into an Arrow Function (2-1) Code and notes

--- React Fundamentals/Lesson 1 Why react/ES6/Lesson 2 Functions/app.js
+++ React Fundamentals/Lesson 1 Why react/ES6/Lesson 2 Functions/app.js
@@ -1,8 +1,26 @@
 'use strict';
 // console.log('hi yehia') //test File
 
+/*
 //Arrow Functions
 const upperizedNames = ['Farrin', 'Kagure', 'Asser'].map(
     name => name.toUpperCase()
 );
-console.log(upperizedNames)
+console.log(upperizedNames)*/
+//Quiz: Convert Function into an Arrow Function (2-1)
+/*
+* Before modification
+
+
+// convert to an arrow function
+const squares = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map(function(square) {
+    return square * square;
+});
+
+console.log(...squares);
+*/
+//After modification to arrow function
+const squares = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map(square => square * square);
+
+console.log(...squares);
+
