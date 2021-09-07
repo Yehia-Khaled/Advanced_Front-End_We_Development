9ee2ecd3bfb9a55cf830fe230f80283e2f6cf960
Yehia Khaled <55062155+Yehia-Khaled@users.noreply.github.com>
2021-09-07 16:44:32 +0200

    11. Quiz: Using Default Function Parameters (2-2) Code and notes

--- React Fundamentals/Lesson 1 Why react/ES6/Lesson 2 Functions/app.js
+++ React Fundamentals/Lesson 1 Why react/ES6/Lesson 2 Functions/app.js
@@ -7,8 +7,9 @@ const upperizedNames = ['Farrin', 'Kagure', 'Asser'].map(
     name => name.toUpperCase()
 );
 console.log(upperizedNames)*/
-//Quiz: Convert Function into an Arrow Function (2-1)
 /*
+//Quiz: Convert Function into an Arrow Function (2-1)
+/!*
 * Before modification
 
 
@@ -18,9 +19,25 @@ const squares = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map(function(square) {
 });
 
 console.log(...squares);
-*/
+*!/
 //After modification to arrow function
 const squares = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map(square => square * square);
 
 console.log(...squares);
+*/
 
+//11. Quiz: Using Default Function Parameters (2-2)
+/*
+ * Programming Quiz: Using Default Function Parameters (2-2)
+ */
+
+const buildHouse=function ({floors=1,color='red',walls='bricks'}={}){
+    return `Your house has ${floors} floor(s) with ${color} ${walls} walls.`
+}
+
+// your code goes here
+
+ // tests
+console.log(buildHouse()); // Your house has 1 floor(s) with red brick walls.
+console.log(buildHouse({})); // Your house has 1 floor(s) with red brick walls.
+console.log(buildHouse({floors: 3, color: 'yellow'})); // Your house has 3 floor(s) with yellow brick walls.
