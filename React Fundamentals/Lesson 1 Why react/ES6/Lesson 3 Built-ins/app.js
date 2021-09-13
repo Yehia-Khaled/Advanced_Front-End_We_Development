43088ba2d67d1753421433aa2b9e21c038c9c5a0
Yehia Khaled <55062155+Yehia-Khaled@users.noreply.github.com>
2021-09-13 10:08:59 +0200

    11- Quiz: Working With WeakSets Code and notes

--- React Fundamentals/Lesson 1 Why react/ES6/Lesson 3 Built-ins/app.js
+++ React Fundamentals/Lesson 1 Why react/ES6/Lesson 3 Built-ins/app.js
@@ -1,6 +1,6 @@
 'use strict';
-//9- Quiz: Using Sets
-/*
+/*//9- Quiz: Using Sets
+/!*
  * Programming Quiz: Using Sets (3-1)
  *
  * Create a Set object and store it in a variable named `myFavoriteFlavors`. Add the following strings to the set:
@@ -10,7 +10,7 @@
  *     - vanilla
  *
  * Then use the `.delete()` method to remove "strawberry" from the set.
- */
+ *!/
 //create a set of object
 const myFavoriteFlavors= new Set();
 
@@ -24,5 +24,34 @@ console.log(myFavoriteFlavors);
 //delete items on set
 myFavoriteFlavors.delete('strawberry');
 // required set
-console.log(myFavoriteFlavors);
+console.log(myFavoriteFlavors);*/
+
+//11- Quiz: Working With WeakSets
+
+/*
+ * Programming Quiz: Using Sets (3-2)
+ *
+ * Create the following variables:
+ *     - uniqueFlavors and set it to a new WeakSet object
+ *     - flavor1 and set it equal to `{ flavor: 'chocolate' }`
+ *     - flavor2 and set it equal to an object with property 'flavor' and value of your choice!
+ *
+ * Use the `.add()` method to add the objects `flavor1` and `flavor2` to `uniqueFlavors`
+ * Use the `.add()` method to add the `flavor1` object (again!) to the `uniqueFlavors` set
+ */
+// create uniqueFlavors and set it to a new WeakSet object
+const uniqueFlavors =new WeakSet();
+
+//create objects and assign to values flavor 1 flavor 2
+const flavor1={flavor:'chocolate'};
+const flavor2={flavor:'Vanilla'};
+
+//use add method to add flavors
+uniqueFlavors.add(flavor1)
+uniqueFlavors.add(flavor2)
+console.log(uniqueFlavors)//test data before add flavor 1 again
+uniqueFlavors.add(flavor1)
+console.log(uniqueFlavors)//test data after add flavor 1 again
+
+
 
