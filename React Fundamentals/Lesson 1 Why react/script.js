f29238a694abd8b917bb55ff19aebbccd7a0f841
Yehia Khaled <55062155+Yehia-Khaled@users.noreply.github.com>
2021-09-15 09:10:22 +0200

    5. React Is “just JavaScript” .filter() and .map() Quiz Code and notes

--- React Fundamentals/Lesson 1 Why react/script.js
+++ React Fundamentals/Lesson 1 Why react/script.js
@@ -39,8 +39,10 @@ const albumSalesStrings = musicData.map(album => `${album.name} by ${album.artis
 
 console.log(albumSalesStrings);
 */
+/*
+
 //.filter() Quiz
-/* Using .filter()
+/!* Using .filter()
  *
  * Using the musicData array and .filter():
  *   - return only album objects where the album's name is
@@ -50,7 +52,7 @@ console.log(albumSalesStrings);
  * Note:
  *   - do not delete the musicData variable
  *   - do not alter any of the musicData content
- */
+ *!/
 
 const musicData = [
     {artist: 'Adele', name: '25', sales: 1731000},
@@ -74,7 +76,7 @@ const results = musicData.filter((album) => album.name.length >= 10 && album.nam
 // const results = musicData.filter(album => album.name.length <= 25 && album.name.length >= 10);
 
 console.log(results);
-/*[ { artist: 'Pentatonix',
+/!*[ { artist: 'Pentatonix',
     name: 'A Pentatonix Christmas',
     sales: 904000 },
   { artist: 'Twenty One Pilots',
@@ -82,4 +84,45 @@ console.log(results);
     sales: 738000 },
   { artist: 'Prince',
     name: 'The Very Best of Prince',
-    sales: 668000 } ]*/
+    sales: 668000 } ]*!/*/
+
+//.filter() and .map() Quiz
+/* Combining .filter() and .map()
+ *
+ * Using the musicData array, .filter, and .map():
+ *   - filter the musicData array down to just the albums that have
+ *     sold over 1,000,000 copies
+ *   - on the array returned from .filter(), call .map()
+ *   - use .map() to return a string for each item in the array in the
+ *     following format: "<artist> is a great performer"
+ *   - store the array returned form .map() in a new "popular" variable
+ *
+ * Note:
+ *   - do not delete the musicData variable
+ *   - do not alter any of the musicData content
+ */
+
+const musicData = [
+    {artist: 'Adele', name: '25', sales: 1731000},
+    {artist: 'Drake', name: 'Views', sales: 1608000},
+    {artist: 'Beyonce', name: 'Lemonade', sales: 1554000},
+    {artist: 'Chris Stapleton', name: 'Traveller', sales: 1085000},
+    {artist: 'Pentatonix', name: 'A Pentatonix Christmas', sales: 904000},
+    {
+        artist: 'Original Broadway Cast Recording',
+        name: 'Hamilton: An American Musical', sales: 820000
+    },
+    {artist: 'Twenty One Pilots', name: 'Blurryface', sales: 738000},
+    {artist: 'Prince', name: 'The Very Best of Prince', sales: 668000},
+    {artist: 'Rihanna', name: 'Anti', sales: 603000},
+    {artist: 'Justin Bieber', name: 'Purpose', sales: 554000}
+];
+
+const popular = musicData.filter((album) => album.sales > 1000000).map((album) => `${album.artist} is a greater performer 💪`);
+
+console.log(popular);
+/*output
+* [ 'Adele is a greater performer 💪',
+  'Drake is a greater performer 💪',
+  'Beyonce is a greater performer 💪',
+  'Chris Stapleton is a greater performer 💪' ]*/
